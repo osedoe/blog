a09b0dbfe720989ff07d9ac4905dac2c0ec0bd9e
Ose Diaz Gonzalez <jose.diazg@protonmail.com>
2020-12-05 15:02:49 +0000

    Remove public/

--- public/styles-e8fd552804e2970df7d0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{178:function(n,o,w){},179:function(n,o,w){},209:function(n,o,w){}}]);
-//# sourceMappingURL=styles-e8fd552804e2970df7d0.js.map
