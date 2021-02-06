330832872749690257d1a0e0df8300d1502f0650
Ose Diaz Gonzalez <jose.diazg@protonmail.com>
2021-02-06 17:13:43 +0000

    Check

--- src/components/header/nav/menu/MobileMenu.tsx
+++ src/components/header/nav/menu/MobileMenu.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react';
 import styled from '@emotion/styled';
 
 const Container = styled.div`
-
+  display: fixed;
+  width: 100vw;
+  border: 2px solid white;
 `;
 
 export const MobileMenu: FC = () => {
-    return <Container>
-
-    </Container>;
+  return <Container>
+    123
+  </Container>;
 };
